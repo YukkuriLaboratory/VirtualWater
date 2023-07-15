27b7cfe4cb257333d4c4f53cc328a01680f4e76d
turtton <top.gear7509@turtton.net>
2023-07-15 20:20:21 +0900

    fix: Support cave grasses

--- common/src/main/java/net/yukulab/virtualpump/mixin/waterproof/MixinPropertyInjectableBlock.java
+++ common/src/main/java/net/yukulab/virtualpump/mixin/waterproof/MixinPropertyInjectableBlock.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin({SaplingBlock.class, TallPlantBlock.class, TorchflowerBlock.class, BeetrootsBlock.class, CropBlock.class, SnowBlock.class, SugarCaneBlock.class, VineBlock.class, NetherWartBlock.class, CocoaBlock.class, AbstractPlantStemBlock.class, BambooBlock.class, FlowerbedBlock.class})
+@Mixin({SaplingBlock.class, TallPlantBlock.class, TorchflowerBlock.class, BeetrootsBlock.class, CropBlock.class, SnowBlock.class, SugarCaneBlock.class, VineBlock.class, NetherWartBlock.class, CocoaBlock.class, AbstractPlantStemBlock.class, BambooBlock.class, FlowerbedBlock.class, CaveVinesBodyBlock.class})
 public abstract class MixinPropertyInjectableBlock {
     @Inject(
             method = "appendProperties",
