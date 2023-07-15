eee7b771e376507f7c315e54790e39712abdd3b0
turtton <top.gear7509@turtton.net>
2023-07-15 13:33:06 +0900

    feat: Prevent grass block broken

---
+++ common/src/main/java/net/yukulab/virtualpump/mixin/MixinSpreadableBlock.java
@@ -0,0 +1,30 @@
+package net.yukulab.virtualpump.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SpreadableBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(SpreadableBlock.class)
+public class MixinSpreadableBlock {
+    @Inject(
+            method = "canSurvive",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/fluid/FluidState;getLevel()I"
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true
+    )
+    private static void ignoreWater(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir, BlockPos blockPos, BlockState belowBlock) {
+        if (belowBlock.isOf(Blocks.WATER)) {
+            cir.setReturnValue(true);
+        }
+    }
+}
